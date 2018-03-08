a4a9c6d69e8ec74f39931e8315d46b73fee5229c
mediajolt001 <j@jolton.com>
2018-03-08 15:49:46 -0600

    Initial commit

---
+++ Assets/__Scripts/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LearnProgrammingAcademy.Learning
+{
+    public class Demo : MonoBehaviour
+    {
+        int lives = 3;
+        string playerName = "Karl";
+        bool gameOver; // false by default
+
+
+        private void Start()
+        {
+            print("Hello");
+            Debug.Log("Hello Log");
+
+            Debug.Log("player " + playerName + " has " + lives + " lives left.");
+            Debug.LogFormat("Player {0} has {1} lives left", playerName, lives);
+        }
+
+    }
+
+}
+
