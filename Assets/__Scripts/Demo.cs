56c851841eb9bdb19e8c04675a6eaea2477d771f
mediajolt001 <j@jolton.com>
2018-03-19 15:52:29 -0500

    for loops and arrays

--- Assets/__Scripts/Demo.cs
+++ Assets/__Scripts/Demo.cs
@@ -6,30 +6,40 @@ namespace LearnProgrammingAcademy.Learning
 {
     public class Demo : MonoBehaviour
     {
-        int lives = 3;
-        int score;
-        int coinCount = 90;
-        bool gameOver;
+        int[] names = new int[4];   //initialize empty array with 4 elements
 
-        private void Start()
+        string[] pets = { "parrot", "cat", "dog" }; //anonymous array
+
+        int[,] table = new int[3, 3]; // 3x3 array
+
+        // jagged array of arrays
+        string[][] myArray =
         {
-           // lives = 0;
-            while ( lives > 0)
-            {
-                Debug.LogFormat("lives =  {0}", lives);
-                lives--;
-            }
-            Debug.Log("after while loop");
-            lives = 3;
+            new string[]{"one","two"}, //1st row has 2 elements
+            new string[] {"three","four","five"} //2nd row of three
+        };
 
-            do
-            {
-                coinCount += 20;
-                lives--;
-            } while (lives > 0);
 
-            Debug.LogFormat("coins = {0}, lives = {1}", coinCount, lives);
+         private void Start()
+        {
+            Debug.LogFormat("pets-size = {0}", pets.Length);
+
+            Debug.LogFormat("pets[0] = {0}", pets[0]);
+            Debug.LogFormat("pets[1] = {0}", pets[1]);
+            Debug.LogFormat("pets[2] = {0}", pets[2]);
+            //Debug.LogFormat("pets[3] = {0}", pets[3]); //out of range
+
+            table[0, 0] = 10;
+            table[1, 1] = 20;
+
+            Debug.LogFormat("table[0,0] = {0}", table[0, 0]);
+            Debug.LogFormat("table[1,1] = {0}", table[1, 1]);
+            Debug.LogFormat("table[2,2] = {0}", table[2, 2]);
+            //Debug.LogFormat("table[3,2] = {0}", table[3, 2]); // out of range
+
+
         }
+
     }
 }
 
---
+++ Assets/__Scripts/Demo1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LearnProgrammingAcademy.Learning
+{
+    public class Demo1 : MonoBehaviour
+    {
+         string[] pets = { "parrot", "cat", "dog" }; //anonymous array
+
+        int[,] table = {
+            {1,2,3 },
+            {4,5,6 },
+            {7,8,9},
+        };
+
+         private void Start()
+        {
+            for (int i = 0; i < pets.Length; i ++)
+                Debug.LogFormat("pet[{0}] = {1}", i, pets[i]);
+
+            foreach (string pet in pets)
+            {
+                Debug.LogFormat("pet ={0}", pet);
+            }
+
+            foreach(int number in table)
+            {
+                Debug.LogFormat("number = {0}", number);
+            }
+        }
+
+    }
+}
+
